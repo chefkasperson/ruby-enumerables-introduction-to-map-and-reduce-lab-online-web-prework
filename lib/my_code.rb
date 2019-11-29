577317405f3d96312dc669011ef89106743cb5d8
Erik Kasperson <chefkasperson@gmail.com>
2019-11-29 04:38:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,3 +21,28 @@ def map_to_no_change(array)
   end
   a 
 end 
+
+def map_to_double(array)
+  a = []
+  i = 0 
+  
+  while i < array.length do 
+    x = array[i] * 2 
+    a << x 
+    i += 1 
+  end
+  a 
+end
+
+def map_to_square(array)
+  a = []
+  i = 0 
+  
+  while i < array.length do 
+    x = array[i] * array[i]
+    a << x 
+    i += 1 
+  end
+  a 
+end 
+
